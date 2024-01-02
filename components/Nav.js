bf3e3e7d2bdaaaf9c33ad6bfcc21dc580d8f5736
Stephen <stephenhuang8@gmail.com>
2024-01-02 13:35:10 -0800

    added layout and nav

--- components/Nav.js
+++ components/Nav.js
@@ -5,17 +5,17 @@ export default function Nav() {
   return (
     <header className="border-b sticky top-0 z-20 bg-white">
       <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
-        <Link href="/" passHref>
-            <a className="cursor-pointer">
-                <span className="text-lg pt-1 font-bold">
-                    Shopify + Next.js
-                </span>
-            </a>
+        <Link href="/" passHref className="cursor-pointer">
+
+          <span className="text-lg pt-1 font-bold">
+              Shopify + Next.js
+          </span>
+
         </Link>
         <a className="text-md font-bold cursor-pointer">
             Cart
         </a>
       </div>
     </header>
-  )
+  );
 }
