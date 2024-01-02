2c71eab74eb6332a8415d9a10cfa5868a0484a65
Stephen <stephenhuang8@gmail.com>
2024-01-02 13:28:38 -0800

    Added products to home page, clickable links to product page

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex flex-col justify-between min-h-screen">
+        <Nav />
+        <main>
+            {children}
+        </main>
+    </div>
+  )
+}
--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,20 +1,23 @@
 import Link from "next/link"
 import Image from "next/image"
+import { formatter } from "../utils/helpers"
 
 const ProductCard = ({ product }) => {
     const { handle, title } = product.node
 
-    const { altText, originalSrc } = product.node.images.edges[0].node
+    const { altText, url } = product.node.images.edges[0].node
+
+    const price = product.node.priceRange.minVariantPrice.amount
 
     return (
-        <Link href={`/product/${handle}`}>
-            <a className="group">
+        <Link className="group" href={`/products/${handle}`}>
                 <div className="w-full bg-gray-200 rounded-3xl overflow-hidden">
                     <div className="relative group-hover:opacity-75 h-72">
-                        <Image src={originalSrc} alt={altText} layout="fill" objectFit="cover"/>
+                        <Image src={url} alt={altText} layout="fill" objectFit="cover"/>
                     </div>
                 </div>
-            </a>
+                <h3 className="mt-4 text-lg font-medium text-gray-900">{title}</h3>
+                <p className="mt-1 text-sm text-gray-700">{formatter.format(price)}</p>
         </Link>
   )
 }
