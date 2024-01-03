ed30e0f1ac5162f881565dbdbdd1f96cc8cbf253
Stephen <stephenhuang8@gmail.com>
2024-01-03 01:58:26 -0800

    Operating Product Page. Working on carting

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,4 +1,6 @@
 import { useState, useContext } from "react"
+import { formatter } from "../utils/helpers"
+import ProductOptions from "./ProductOptions"
 
 export default function ProductForm({ product }) {
     console.log(product)
@@ -9,12 +11,11 @@ export default function ProductForm({ product }) {
         variant.node.selectedOptions.map(item => {
             allOptions[item.name] = item.value
         })
-        console.log(variant.node.product)
 
         return {
             id: variant.node.id,
-            title: variant.node.product,
-            handle: variant.node.product,
+            title: product.title,
+            handle: product.handle,
             image: variant.node.image?.originalSrc,
             options: allOptions,
             variantTitle: variant.node.title,
@@ -23,9 +24,36 @@ export default function ProductForm({ product }) {
         }
     })
 
-    return (
-        <div>
+    const defaultValues = {}
+    product.options.map(item => {
+        defaultValues[item.name] = item.values[0]
+    })
 
+    const [selectedVariant, setSelectedVariant] = useState(allVariantOptions[0])
+    const [selectedOptions, setselectedOptions] = useState(defaultValues)
+
+    function setOptions (name, value) {
+        setselectedOptions(prevState => {
+            return { ...prevState, [name]: value }
+        })
+    }
+
+    return (
+        <div className="rounded-2xl p-4 shadow-lg flex flex-col w-full md:w-1/3">
+            <h2 className="text-2xl font-bold">{product.title}</h2>
+            <span className="pb-6">{formatter.format(product.variants.edges[0].node.price.amount)}</span>
+            {
+                product.options.map(({ name, values}) => (
+                    <ProductOptions
+                        key={`key-${name}`}
+                        name={name}
+                        values={values}
+                        selectedOptions={selectedOptions}
+                        setOptions={setOptions}
+                    />
+                ))
+            }
+            <button className="bg-black rounded-lg text-white px-2 py-3 hover:bg-gray-800">Add to Cart</button>
         </div>
     )
 }
