24287898eb4ffc69418afbf3db524ca3ef71765e
Stephen <stephenhuang8@gmail.com>
2024-01-03 15:22:07 -0800

    Updated mini cart

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
-import "../styles/global.css"
-import Layout from "../components/Layout"
+import 'tailwindcss/tailwind.css'
+import Layout from '../components/Layout'
+import ShopProvider from '../context/ShopContext'
+import { useRouter } from 'next/router'
 
-export default function MyApp({ Component, pageProps }) {
-    return (
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  
+  return (
+    <ShopProvider>
       <Layout>
-        <Component {...pageProps} />
+        <Component {...pageProps} key={router.asPath} />
       </Layout>
-    )
-  }
+    </ShopProvider>
+  )
+}
+
+export default MyApp
