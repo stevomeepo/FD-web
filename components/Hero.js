fba633dc04805ae36ab451b306ea4556e26e959e
stevomeepo <stephenhuang8@gmail.com>
2024-01-08 14:22:40 -0800

    WIP carousel for hero page

--- components/Hero.js
+++ components/Hero.js
@@ -1,23 +1,34 @@
 import Link from 'next/link'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function Hero() {
   return (
-    <div className="my-48 mx-auto max-w-7xl px-4 sm:mt-24 md:mt-72 text-center">
-      <h1 className="font-extrabold text-gray-900">
-        <p className="text-xl sm:text-3xl md:text-4xl">Welcome to Forensic Drone</p>
-        <p className="text-transparent bg-clip-text bg-gradient-to-r from-red-400 via-red-500 to-red-500 text-4xl sm:text-6xl md:text-7xl">Home of Forensic Hardware</p>
-      </h1>
-      <h2 className="mt-3 max-w-md mx-auto text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-x-3xl">
-        Provider of cutting-edge forensic hardware and software for computer forensic expoerts in the law enforcement, education, military, civil, or corporate field.
-      </h2>
-      <div className="mt-5 max-w-md mx-auto flex justify-center items-center md:mt-8">
-        <a href="https://buildnextshop.com" className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium py-3 border-transparent rounded-md text-white bg-gray-900 hover:bg-gray-800">
-          Enroll Now
-        </a>
-        <a href="https://buildnextshop.com" className="inline-flex items-center font-semibold text-gray-900 hover:text-gray-800">
-          Learn more
-        </a>
+    <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col items-center space-y-8 text-center">
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold tracking-tighter text-black sm:text-4xl md:text-5xl lg:text-6xl">
+              Discover the Future of Forensic Technology
+            </h1>
+          </div>
+          <Carousel autoPlay infiniteLoop>
+            <div>
+              <img className="w-1/3 object-contain" src="https://cdn.shopify.com/s/files/1/0852/4529/6941/files/faraday_tent.png?v=1704752237" />
+              <p className="legend">FD-FT8 Signal Armor Faraday Tent</p>
+            </div>
+          </Carousel>
+          <div className="space-x-4">
+            <Link
+              className="px-8 rounded-r-md bg-red-500 hover:bg-black text-white hover:text-white font-bold p-2 uppercase border-red border-t border-b border-r"
+              href="/about-us"
+            >
+              Learn More
+            </Link>
+          </div>
+        </div>
+        <div className="h-1 bg-red-600 mt-6" />
       </div>
-    </div>
+    </section>
   )
 }
