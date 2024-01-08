21e7657b892280923de5670ca4ceef055e8fbf4c
stevomeepo <stephenhuang8@gmail.com>
2024-01-08 14:22:40 -0800

    WIP carousel for hero page

---
+++ pages/about-us.js
@@ -0,0 +1,13 @@
+export default function AboutUs() {
+    return (
+        <div className="flex flex-col items-center justify-center prose prose-lg mx-auto p-4 max-w-2xl text-center">
+            <h1 className="text-4xl font-bold pb-7">About Us</h1>
+            <p>ForensicDrone is a small, private, and family owned business. 
+                Founded in 2011 with the mission of building fast and reliable forensic systems for law enforcements, our goal is to provide assistance to forensic evidence from collected source data.</p>
+            <h1 className="text-4xl font-bold pb-7">Experience</h1>
+            <p>ForensicDrone takes great pride and joy in our highly skilled team. 
+                Our team has over 20 years of experience in building technical and forensic systems. 
+                We have proven our excellent contribution and knowledge to designing forensic hardware, software, and infrastructure.</p>
+        </div>
+    )
+}
