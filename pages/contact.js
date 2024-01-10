4b42f99be6e31772c41b944bbef88b0ad4dfbfb8
Stephen <stephenhuang8@gmail.com>
2024-01-10 00:19:47 -0800

    Updated image for contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,17 +1,18 @@
+import Image from 'next/image';
+
 export default function Contact() {
     return (
         <div className="bg-black-600 min-h-screen flex items-center justify-center">
             <div className="container mx-auto p-4 flex flex-wrap items-start">
-                {/* Contact Information */}
                 <div className="w-full lg:w-1/2 p-4">
-                    <h2 className="text-4xl font-bold text-white mb-4">CONTACT US</h2>
-                    <div className="text-white">
+                    <h2 className="text-4xl font-bold text-black mb-4">CONTACT US</h2>
+                    <div className="text-black">
                         <p>1200 N. Van Buren St. STE A</p>
                         <p>Anaheim, CA 92807</p>
-                        <p className="my-4">Call us 714.238.8888</p>
+                        <p className="my-4">Call us (714)-238-8888</p>
                     </div>
                     <div className="mt-8">
-                        {/* Image here */}
+                        <Image src="https://cdn.shopify.com/s/files/1/0852/4529/6941/files/IMG_0649.jpg?v=1704874587" width={550} height={550}/>
                     </div>
                 </div>
                 {/* Contact Form */}
@@ -30,7 +31,7 @@ export default function Contact() {
                             <div className="mb-4">
                                 <textarea placeholder="Type your message here..." className="w-full p-2 border border-gray-300 rounded h-32"></textarea>
                             </div>
-                            <button type="submit" className="w-full p-3 bg-black text-white rounded hover:bg-red-800">Submit</button>
+                            <button type="submit" className="w-full p-3 bg-black text-white rounded hover:bg-red-500">Submit</button>
                         </form>
                     </div>
                 </div>
