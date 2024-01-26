1630d9c6503741e73a868201f9c1f2c59bbe651b
stevomeepo <stephenhuang8@gmail.com>
2024-01-26 13:58:22 -0800

    Fixing signup issue

--- components/SignUp.js
+++ components/SignUp.js
@@ -39,7 +39,7 @@ export default function Signup() {
       return;
     }
 
-    const response = await fetch('/api/SignUp', {
+    const response = await fetch('/api/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
