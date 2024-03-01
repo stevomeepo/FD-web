f3e0ec79d6925d1dd71d7b2c4ea99707467d367a
stevomeepo <stephenhuang8@gmail.com>
2024-02-29 17:34:21 -0800

    Changed text font

--- pages/contact.js
+++ pages/contact.js
@@ -95,17 +95,15 @@ export default function Contact() {
                 </div>
                 {error && <p>{error}</p>}
                 <div className="flex justify-center">
-                <button type="submit" className="contact-submit-button flex items-center justify-center space-x-2" onClick={handleSubmit}>
-                  <div className="svg-wrapper-1">
-                    <div className={`svg-wrapper flex items-center justify-center ${isSubmitting ? 'icon-animating icon-rotated' : ''}`} >
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className={`fill-current ${isSubmitting ? 'icon-rotated' : ''}`}>
+                  <button type="submit" className="contact-submit-button bg-black hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" onClick={handleSubmit}>
+                    <div className="svg-wrapper">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="fill-current">
                         <path fill="none" d="M0 0h24v24H0z"></path>
                         <path d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
                       </svg>
                     </div>
-                  </div>
-                  <span className={`submit-text ${isSubmitting ? 'submit-text-animating' : ''}`}>Submit</span>
-                </button>
+                    <span className="submit-text">Submit</span>
+                  </button>
                 </div>
               </form>
             )}
